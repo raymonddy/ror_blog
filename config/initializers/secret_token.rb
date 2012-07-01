680349589b68bec04300a1b1830e8165e9444532
Dong Yang <raymonddy@gmail.com>
2012-07-01 21:00:23 +0900

    initialization of the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '86173def270b2e66e47a72885458192d66234535330fee74e08b2d6ec4490a3266781b63eec05d372fc9d3e89771ed93f81f00e52d27b9c3ae18bf9265e38dfb'
